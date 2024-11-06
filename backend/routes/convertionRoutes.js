85d34706b8cd3d5bd8618d203678f1ed92d6fcb0
Miguel Jose Mata Ramos <miguelmodd@gmail.com>
2024-11-06 00:23:57 -0400

    adding some json validations

--- backend/routes/convertionRoutes.js
+++ backend/routes/convertionRoutes.js
@@ -4,7 +4,13 @@ const controller = require('../controllers/convertionController')
 const routes = express.Router()
 const routePath = '/api/convertion'
 
-routes.post(routePath, controller.post)
+const { Validator } = require('express-json-validator-middleware')
+const convertionSchema = require('../validatorSchema/convertionSchema')
+// eslint-disable-next-line new-cap
+const validator = new Validator()
+const validate = validator.validate
+
+routes.post(routePath, validate(convertionSchema), controller.post)
 routes.get(`${routePath}/:id`, controller.getConvertionById)
 routes.get(routePath, controller.getAllConvertionHistories)
 
